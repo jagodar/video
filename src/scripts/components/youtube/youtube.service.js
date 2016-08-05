b1b3a06fd1ec42f9b3d5bb7728a833f1f96a573d
jagoda <jagoda.er@gmail.com>
2016-08-05 17:58:29 +0200

    & AJAX

--- src/scripts/components/youtube/youtube.service.js
+++ src/scripts/components/youtube/youtube.service.js
@@ -1,12 +1,39 @@
 class YoutubeService {
 
-	search(query, callback) {
-		var xhttp = new XMLHttpRequest();
-		xhttp.open('GET', 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=' + query + '&type=video&key=AIzaSyA8ngE7e1236movhXfRbRORTV7NbvuhomU',true);
-		xhttp.send();
-		var response = xhttp.responseText;
-		console.log(response);
+	constructor($http) {
+		this.$http = $http;
 	}
+
+	// AJAX
+
+	searchAJAX(query, callback) {
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (xhttp.readyState == 4 && xhttp.status == 200) {
+				var response = xhttp.responseText;
+				console.log(response);
+			}
+		}
+		xhttp.open('GET', 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=' + query + '&type=video&key=AIzaSyA8ngE7e1236movhXfRbRORTV7NbvuhomU', true);
+		xhttp.send();	
+	}
+
+	// Angular 
+
+	 search(query, callback) {
+		this.$http({
+			method: 'GET',
+			url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=' + query + '&type=video&key=AIzaSyA8ngE7e1236movhXfRbRORTV7NbvuhomU'
+		}).then(function successCallback(response) {
+			console.log(response.status);
+			console.log(response.data);
+		}, function errorCallback(response) {
+			console.log(response.status);
+		})
+	}
+
 }
+
+YoutubeService.$inject = ['$http'];
 
 export default YoutubeService;
